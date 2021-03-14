319d84def7076ecda5f59a1d23c19e45e660664b
NilEis <52820950+NilEis@users.noreply.github.com>
2021-03-14 14:26:05 +0100

    Update main.asm

--- src/main.asm
+++ src/main.asm
@@ -1,3 +1,4 @@
+[bits 16]                 ; Set 16 bit mode
 [org 0x7c00]              ; Offset added to all adresses
 
 ; Set 80x25 16-color text mode
@@ -14,7 +15,7 @@ jmp $                     ; Infinite loop
 %include "src/screen.asm"
 
 hwString db  'Hello world'
-hwLength db 11
+hwLength db ($-hwString)
 times 510-($-$$) db 0     ; Fill rest of bootsector with 0
 dw 0xaa55                 ; Set the last to 0xaa55
 times 1474560-($-$$) db 0 ; Resize file to 1.4 mb (Floppy)
