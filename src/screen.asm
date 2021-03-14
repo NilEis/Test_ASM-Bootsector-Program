42aa879a7fb06b4e0ac963be515becc8a8be634b
NilEis <52820950+NilEis@users.noreply.github.com>
2021-03-14 15:40:15 +0100

    Update screen.asm

--- src/screen.asm
+++ src/screen.asm
@@ -16,3 +16,22 @@ ret
 exec_BASIC:
     int 18h
 ret
+
+set_16_color_text_mode:; Set 80x25 16-color text mode
+    xor ah, ah                ; Set ah = 0
+    mov al, 0x03              ; Set instruction 0x03
+    int 0x10                  ; Call int 0x10
+ret
+
+set_vga_mode:
+    mov ax, 13h ; AH=0 (Change video mode), AL=13h (Mode)
+    int 10h     ; Video BIOS interrupt
+ret
+
+; set al to the colour
+; set cx to y
+; set dx to x
+set_pixel:
+    mov ah, 0x0C
+    int 10h
+ret
